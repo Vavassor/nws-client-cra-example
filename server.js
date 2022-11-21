bb3834fea131785175ad365b3e2095a0f1dad53f
Andrew Dawson <dawso.andrew@gmail.com>
2022-11-20 20:12:01 -0500

    Fixes an issue with serving the react app.

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const router = express.Router();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname, "app", "build")));
 
-router.get("*", function (request, response) {
-  response.sendFile(path.join(__dirname, "./app/build/index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "app", "build", "index.html"));
 });
-
-app.use("/", router);
 
 app.listen(process.env.PORT || 8080);
